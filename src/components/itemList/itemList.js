0625bf2e4a3a943a3754eb9b0f9394b46036de01
Antonina Berchuk <tonyaberch@gmail.com>
2021-08-02 20:11:55 +0200

    feat: make ItemList on hooks

--- src/components/itemList/itemList.js
+++ src/components/itemList/itemList.js
@@ -1,52 +1,44 @@
-import React, {Component} from 'react';
+import React,{useState, useEffect} from 'react';
 import './itemList.css';
 import Spinner from '../spinner';
 
-export default class ItemList extends Component {
-    constructor(props) {
-        super(props);
-      
-        this.state = {
-           itemList: null,
-        }
-    }
 
-    componentDidMount() {
-        const { getData } = this.props;
-        getData()
-            .then((itemList) => {
-                this.setState({ itemList })
-            });
-    }
+function ItemList({getData, onItemSelected, renderItem}){
+    const [itemList, updateList]=useState([]);
+    
+    useEffect(()=>{
+       getData()
+            .then((data) => {
+              updateList(data);
+            }); 
+    }, [])
+    
 
-    renderItems(arr) {
+    function renderItems(arr) {
         
         return arr.map((item, index) => {
             const id = +/\d+/.exec(item.url)
-            const label = this.props.renderItem(item);
+            const label = renderItem(item);
             return (
                 <li
                     key={index}
                     className="list-group-item"
-                    onClick={()=>this.props.onItemSelected(id)}
-                >
+                    onClick={()=>onItemSelected(id)}>
                     {label}
                 </li>
            )
        })
     }
 
-    render() {
-        const { itemList } = this.state;
-
-        if (!itemList) {
-            return <Spinner/>            
-        }
-
-        return (
-            <ul className="item-list list-group">
-               {this.renderItems(itemList)}
-            </ul>
-        );
+    if (!itemList) {
+        return <Spinner/>            
     }
+
+    return (
+        <ul className="item-list list-group">
+           {renderItems(itemList)}
+        </ul>
+    );    
 }
+
+export default ItemList;
